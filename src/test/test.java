e2896b5ba11e8093a69a8d82b03889028f21db70
naoto-mori <naofto@gmail.com>
2020-04-24 18:22:02 +0900

    master
    master

--- src/test/test.java
+++ src/test/test.java
@@ -4,6 +4,8 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println("second commit");
+		System.out.println("third commit");
 		// TODO 自動生成されたメソッド・スタブ
 
 	}
